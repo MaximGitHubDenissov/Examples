c80785640308592a22f18496a3a5cb2137b08fa0
Maxim Denissov <DM7894289@gmail.com>
2022-09-30 19:28:10 +0600

    add random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,5 @@
-﻿double NumberA = 12;
-double NumberB = 5;
+﻿double NumberA = new Random () .Next (1,10); // Числа от 1,2,3...9
+Console.WriteLine (NumberA);
+double NumberB = new Random () .Next (1,10);
+Console.WriteLine (NumberB);
 Console.WriteLine (NumberA/NumberB);
