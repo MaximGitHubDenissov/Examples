e6a66163fdc07680b3b879a594aafac37280b4df
Maxim Denissov <DM7894289@gmail.com>
2022-09-30 19:38:41 +0600

    add Example005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите имя пользователя");
+string usernsme = Console.ReadLine ();
+
+if (usernsme.ToLower () == "маша")
+{
+    Console.WriteLine ("Ура, это же Маша!");
+} 
+else
+{
+    Console.WriteLine ("Привет, ");
+    Console.WriteLine (usernsme);
+} 
